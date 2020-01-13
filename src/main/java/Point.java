07caa98808a6ab1981c05ef50947a8f2653cf2f5
Daniel-ND <51816130+Daniel-ND@users.noreply.github.com>
2020-01-13 08:27:24 +0300

    Update Point.java

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -5,7 +5,7 @@ import javax.persistence.*;
 @Entity
 public class Point {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int id;
     private double x = 0;
     private double y;
